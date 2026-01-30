0e1e6304f1cf0e983791a5553d9e52d7fac54098
GuNa Skr <Guna.Byrappa@cerner.com>
2026-01-30 22:24:37 +0530

    Fix casing import for POSDeviceController

--- src/routes/posDeviceRoutes.js
+++ src/routes/posDeviceRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/PosDeviceController');
+const controller = require('../controllers/POSDeviceController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.use(protect);
